177668c575e989d28fb71f6a0e0f2ab59e348ad8
KazukiHiraizumi <ca-giken@c-able.ne.jp>
2019-04-09 18:46:39 +0900

    made QC results to show

--- p2p/p2pp.py
+++ p2p/p2pp.py
@@ -9,9 +9,10 @@ from rovi.msg import Floats
 from scipy import optimize
 from sensor_msgs.msg import PointCloud
 from geometry_msgs.msg import Point32
-
-Radius=5
-Goal=0.02
+from std_msgs.msg import Bool
+from std_msgs.msg import String
+from std_msgs.msg import Int32
+from std_msgs.msg import Float64
 
 def np2F(d):  #numpy to Floats
   f=Floats()
@@ -70,6 +71,32 @@ def To3d(uv0,uv1,P0,P1):
       Mt[2][0]*b[0] + Mt[2][1]*b[1] + Mt[2][2]*b[2] + Mt[2][3]*b[3]]
   return np.ravel(WP)
 
+def pub_int(pb,val):
+  dat=Int32()
+  dat.data=int(val)
+  pb.publish(dat)
+  return val
+
+def pub_mm(pb,val):
+  dat=Float64()
+  dat.data=int(val*1000)*0.001
+  pb.publish(dat)
+  return val
+
+def pub_stat():
+  qcnt=Int32(); qcnt.data=len(Result); pb_qcnt.publish(qcnt)
+  print "[points0] [points1] [sigma] [distance]"
+  for col in Result:
+    print int(col[0]),int(col[1]),('%.6f'%col[2]),('%.6f'%col[3])
+  pub_int(pb_pmax,np.max(Result[:,0:2]))
+  pub_int(pb_pmin,np.min(Result[:,0:2]))
+  pub_int(pb_pmean,np.mean(Result[:,0:2]))
+  pub_mm(pb_std,np.max(Result[:,2]))
+  pub_mm(pb_cent,StdVal)
+#  pub_mm(pb_cent,np.mean(Result[:,3]))
+  pub_mm(pb_errh,np.max(Result[:,3])-StdVal)
+  pub_mm(pb_errl,np.min(Result[:,3])-StdVal)
+
 def cb_ps(msg): #callback of ps_floats
   global Result
   Pc=np.reshape(msg.data,(-1,3))
@@ -89,19 +116,24 @@ def cb_ps(msg): #callback of ps_floats
     pnt1=np.hstack((pnt1,np.ones((len(pnt1),1))))
     print "ponits",len(pnt0),len(pnt1)
     pnt=np.vstack((pnt0,pnt1))
-    for cnt in range(5):
+    for cnt in range(1):
       pl=getPlane(pnt)
       dist=getDist(pl,pnt)
       sig=np.std(dist)
       pnt=pnt[np.where(np.abs(dist)<1.5*sig)]
     res=np.array([np.sum(pnt[:,3]==0),np.sum(pnt[:,3]==1),sig,np.abs(pl[3])])
     Result=np.vstack((Result,res))
-    if len(Result)>10: Result=Result[1:]
-    print "[points0] [points1] [sigma] [distance]"
-    for col in Result:
-      print int(col[0]),int(col[1]),('%.6f'%col[2]),('%.6f'%col[3])
+    pub_stat()
+    msg=String(); msg.data=''; pb_message.publish(msg)
   else:
+    msg=String(); msg.data='Point too few'; pb_message.publish(msg)
     print "Too few point"
+
+def cb_abort(msg):
+  global Result
+  if len(Result)>0:
+    Result=np.delete(Result,-1,0)
+  pub_stat()
 
 def cb_posl(msg):
   global PosL
@@ -111,15 +143,39 @@ def cb_posr(msg):
   global PosR
   PosR=np.reshape(msg.data,(-1,2))
 
+def cb_param(msg):
+  global Result,Product_id,StdVal
+  a=eval(msg.data)
+  for key in a:
+    if key is 'product_id':
+      Product_id=a[key]
+      Result=np.array([],dtype=float).reshape((-1,4))
+      qcnt=Int32(); qcnt.data=len(Result); pb_qcnt.publish(qcnt)
+    elif key is 'gage':
+      StdVal=a[key]
+    elif key is 'marker':
+      Radius=a[key]/2
+  return
+
 ###############################################################
 rospy.init_node('p2p',anonymous=True)
-pb_dist=rospy.Publisher("/p2p/dist",numpy_msg(Floats),queue_size=1)
 pb_red=rospy.Publisher("/p2p/red",PointCloud,queue_size=1)
 pb_blue=rospy.Publisher("/p2p/blue",PointCloud,queue_size=1)
+pb_qcnt=rospy.Publisher("/p2p/qcnt",Int32,queue_size=1)
+pb_message=rospy.Publisher("/p2p/message",String,queue_size=1)
+pb_pmean=rospy.Publisher("/p2p/pmean",Int32,queue_size=1)
+pb_pmin=rospy.Publisher("/p2p/pmin",Int32,queue_size=1)
+pb_pmax=rospy.Publisher("/p2p/pmax",Int32,queue_size=1)
+pb_std=rospy.Publisher("/p2p/std",Float64,queue_size=1)
+pb_cent=rospy.Publisher("/p2p/center",Float64,queue_size=1)
+pb_errh=rospy.Publisher("/p2p/errh",Float64,queue_size=1)
+pb_errl=rospy.Publisher("/p2p/errl",Float64,queue_size=1)
 
 rospy.Subscriber("/rovi/ps_floats",numpy_msg(Floats),cb_ps)
 rospy.Subscriber("/rovi/left/marker/pos",numpy_msg(Floats),cb_posl)
 rospy.Subscriber("/rovi/right/marker/pos",numpy_msg(Floats),cb_posr)
+rospy.Subscriber("/p2p/param",String,cb_param)
+rospy.Subscriber("/p2p/abort",Bool,cb_abort)
 
 try:
   Qmat=np.asarray(rospy.get_param('/rovi/genpc/Q')).reshape((4,4))
@@ -129,6 +185,9 @@ except KeyError:
   print "No key"
 
 Result=np.array([],dtype=float).reshape((-1,4))
+Product_id=0
+StdVal=0
+Radius=5
 
 try:
   rospy.spin()
